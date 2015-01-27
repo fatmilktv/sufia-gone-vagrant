99a3398b699f993309a9a715b9d8ec4e0a36d7e9
Kevin Bowrin <kevin.bowrin@carleton.ca>
2015-01-27 16:07:23 -0500

    Install patch

--- ruby.sh
+++ ruby.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Installing ruby..."
-yum -y install ruby
-yum -y install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
+yum -y install ruby 
+yum -y install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel patch
 yum -y install ruby-rdoc ruby-devel
 yum -y install rubygems
 yes | gem update
