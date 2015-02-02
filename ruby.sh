3fe18acd92f3e87f39800c364e5ef09c35e9292c
Al Matthews <amatthews@auctr.edu>
2015-02-02 16:09:50 -0500

    whitespace

--- ruby.sh
+++ ruby.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Installing ruby..."
-yum -y install ruby 
-yum -y install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel gcc-c++ apr-devel postgresql-devel v8-devel apr-util-devel sqlite-devel patch nodejs 
+yum -y install ruby
+yum -y install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel gcc-c++ apr-devel postgresql-devel v8-devel apr-util-devel sqlite-devel patch nodejs
 yum -y install ruby-rdoc ruby-devel
 yum -y install rubygems
 yes | gem update
